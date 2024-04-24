ea8a27c1b4767d6429968a9f9d6f33e786908cff
Julian Maximiliano Smith Magaña <apexmaxgamerpro@gmail.com>
2024-04-24 08:25:27 -0600

    migrations and factory

--- database/factories/ExpenseFactory.php
+++ database/factories/ExpenseFactory.php
@@ -2,17 +2,17 @@
 
 namespace Database\Factories;
 
-use App\Models\Income;
+use App\Models\Expense;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class IncomeFactory extends Factory
+class ExpenseFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
      *
      * @var string
      */
-    protected $model = Income::class;
+    protected $model = Expense::class;
 
     /**
      * Define the model's default state.
@@ -23,10 +23,9 @@ class IncomeFactory extends Factory
     {
         return [
             'user_id' => \App\Models\User::factory(), // Asigna un ID de usuario existente
-            'amount' => $this->faker->randomFloat(2, 100, 5000), // Monto aleatorio entre 100 y 5000
+            'amount' => $this->faker->randomFloat(2, 10, 1000), // Monto aleatorio entre 10 y 1000
             'description' => $this->faker->sentence(), // Descripción aleatoria
             'date' => $this->faker->dateTimeBetween('-1 year', 'now'), // Fecha aleatoria en el último año
         ];
     }
 }
-
--- database/migrations/2024_04_23_030829_create_incomes_table.php
+++ database/migrations/2024_04_23_030829_create_incomes_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('companies', function (Blueprint $table) {
+        Schema::create('incomes', function (Blueprint $table) {
             $table->id();
-						$table->string('name');
-						$table->string('address');
-						$table->string('mobile');
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('companies');
+        Schema::dropIfExists('incomes');
     }
 };
